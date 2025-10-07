70138b2da6b883dcb88c683902a87fb1d21a4851
abdulazizladan <abdulazizladan@gmail.com>
2025-10-07 06:10:06 +0100

    task: Install dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: __dirname + '/../db.sqlite', // Use absolute path to database file
+      entities: [__dirname + '/**/entities/*.entity{.ts,.js}'], // A glob pattern to load your entities
+      synchronize: true, // Auto-create database schema. Use with caution in production!
+      logging: ['query', 'error', 'warn'], // Enable detailed SQL query logging
+      logger: 'advanced-console' // Use advanced console logger
+    }),
+  ]
 })
 export class AppModule {}
