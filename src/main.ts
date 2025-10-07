7223e59de19ce92f318404c578221e1fa09a34b0
abdulazizladan <abdulazizladan@gmail.com>
2025-10-07 05:55:26 +0100

    task: Add swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('NIMASA performance monitoring system')
+    .setDescription('Performance monitoring system')
+    .setVersion('1.0')
+    .addTag('NIMASA')
+    .addTag('performance')
+    .addTag('evaluation')
+    .addTag('CDCU')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
